8f4e486cdeaa58af8c8cdd25c7dad9fec8e6d7d3
Varun Kashyap <varkashp@gmail.com>
2024-02-27 21:30:21 -0800

    Add Java program for validating user input

---
+++ src/ValidatingUserInput.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ValidatingUserInput {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int passes = 0;
+        int failures = 0;
+        int studentCounter = 0;
+
+        while (studentCounter < 10) {
+
+            studentCounter = studentCounter + 1;
+
+            System.out.print("Enter result (1 = pass, 2 = fail): ");
+            int result = input.nextInt();
+
+            if (result == 1) {
+                passes += 1;
+            }
+            else if (result == 2) {
+                failures += 1;
+            }
+            else {
+                System.out.println("Invalid entry");
+                studentCounter = studentCounter - 1;
+            }
+
+        }
+
+        System.out.printf("Passed: %d\n", passes);
+        System.out.printf("Failed: %d\n", failures);
+
+    }
+}
