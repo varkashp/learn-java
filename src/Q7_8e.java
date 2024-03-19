cef8c2eaffdba438914553fd1a8c591ccab13703
Varun Kashyap <varkashp@gmail.com>
2024-03-18 17:15:09 -0700

    Add Java files for PrimeNumber

--- src/Q7_8e.java
+++ src/Q7_8e.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Q7_8e {
 
@@ -8,9 +7,12 @@ public class Q7_8e {
         Scanner input = new Scanner(System.in);
 
         float[] w = new float[99];
-        float[] t = new float[99];
         String exitWord = "";
         int counter = 0;
+        float maxVal;
+        float minVal;
+
+        // Initialize the array
 
         while (counter < 99){
 
@@ -21,24 +23,33 @@ public class Q7_8e {
                 if (counter == 0) {
                     return;
                 }
-                t = new float[counter];
-                System.arraycopy(w, 0, t, 0, counter );
-
                 break;
             }
 
-
             System.out.printf("Enter number for element %d in array f : ", counter);
-            w[counter] += input.nextFloat();
-
+            w[counter] = input.nextFloat();
             counter ++;
 
         }
 
-        Arrays.sort(t);
+        // Find max value
 
-        System.out.printf("\nThe smallest value is %.2f", t[0]);
-        System.out.printf("\nThe largest value is %.2f\n", t[(t.length - 1)]);
+        maxVal = w[0];
+
+        for (int t = 0; t < 98; t ++) {
+            maxVal = Math.max(maxVal, w[t + 1]);
+        }
+
+        // Find max value
+
+        minVal = w[0];
+
+        for (int t = 0; t < 98; t ++) {
+            minVal = Math.min(maxVal, w[t + 1]);
+        }
+
+        System.out.printf("\nThe smallest value is %.2f", minVal);
+        System.out.printf("\nThe largest value is %.2f\n", maxVal);
 
     }
 
