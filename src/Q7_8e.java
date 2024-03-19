cdb163b4af33f1208894b9412e7c846a6351a000
lean-ramki <ramki@withlean.com>
2024-03-18 17:27:57 -0700

    Updates to Exercise 8 Q5

--- src/Q7_8e.java
+++ src/Q7_8e.java
@@ -7,15 +7,13 @@ public class Q7_8e {
         Scanner input = new Scanner(System.in);
 
         float[] w = new float[99];
-        String exitWord = "";
+        String exitWord;
         int counter = 0;
         float maxVal;
         float minVal;
 
         // Initialize the array
-
         while (counter < 99){
-
             System.out.print("\nEnter exit if you want to stop : ");
             exitWord = input.next();
 
@@ -29,27 +27,22 @@ public class Q7_8e {
             System.out.printf("Enter number for element %d in array f : ", counter);
             w[counter] = input.nextFloat();
             counter ++;
-
         }
 
         // Find max value
-
         maxVal = w[0];
-
-        for (int t = 0; t < 98; t ++) {
-            maxVal = Math.max(maxVal, w[t + 1]);
+        for (int t = 1; t < 99; t ++) {
+            maxVal = Math.max(maxVal, w[t]);
         }
 
         // Find max value
-
         minVal = w[0];
-
-        for (int t = 0; t < 98; t ++) {
-            minVal = Math.min(maxVal, w[t + 1]);
+        for (int t = 1; t < 99; t ++) {
+            minVal = Math.min(minVal, w[t]);
         }
 
-        System.out.printf("\nThe smallest value is %.2f", minVal);
-        System.out.printf("\nThe largest value is %.2f\n", maxVal);
+        System.out.printf("The min value is %.2f\n", minVal);
+        System.out.printf("The max value is %.2f\n", maxVal);
 
     }
 
